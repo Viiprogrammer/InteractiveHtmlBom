dfed37e35c0c0523fd30057ea2965c55ff4ea236
Maksim <m_telega@mail.ru>
2023-04-04 11:17:25 +0300

    create language_ru.bat

---
+++ InteractiveHtmlBom/i18n/language_ru.bat
@@ -0,0 +1,12 @@
+::start up echo
+set i18n_gitAddr=                                 https://github.com/openscopeproject/InteractiveHtmlBom
+set i18n_batScar=                                                Bat файл by Scarrrr0725
+set i18n_thx4using=                                      Спасибо вам за то что используете генерацию интерактивного Bom
+
+::convert
+set i18n_draghere=Пожалуйста претяните файл печатной платы EasyEDA сюда:
+set i18n_converting=Конвертация . . . . . .
+
+::converted
+set i18n_again=Хотите сконвертировать еще один файл?
+set i18n_converted=                                    Исходный EDA файл успешно сконвертирован в bom!
